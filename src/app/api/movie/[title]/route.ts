839ddae61633a19bc08a392557bd97d9ee5fd71e
kimggor <dkdjsema9808@naver.com>
2024-08-28 15:29:41 +0900

    커밋

--- src/app/api/movie/[title]/route.ts
+++ src/app/api/movie/[title]/route.ts
@@ -1,38 +1,31 @@
 import { getPrismaClient } from "@/utils/util";
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const { prisma } = getPrismaClient();
   const params = request.nextUrl.searchParams.get("title");
+  console.log(params);
 
-  // params 값이 null인지 확인
-  if (!params) {
-    return NextResponse.json(
-      { message: "Title parameter is required" },
-      { status: 400 },
-    );
-  }
-
+  const findMovie = await prisma.movie.findMany({
+    where: {
+      title: params as string,
+    },
+  });
   try {
-    const findMovie = await prisma.movie.findMany({
-      where: {
-        title: params as string,
-      },
-    });
-
     const findMoviePlace = await prisma.moviePlace.findMany({
       where: {
         title: params as string,
       },
     });
 
-    // 결과를 반환
-    return NextResponse.json({ findMovie, findMoviePlace });
-  } catch (error) {
-    console.error("Error fetching movie data:", error);
-    return NextResponse.json(
-      { message: "Internal server error" },
-      { status: 500 },
+    return (
+      findMoviePlace &&
+      findMovie &&
+      NextResponse.json({ findMovie, findMoviePlace })
     );
+  } catch (error) {
+    console.error(error);
+    NextResponse.json({ message: "Internal server error" });
   }
 }
